0bf7b17fb834a72ddcefde602b1b16f2530af3c2
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-02 09:41:00 -0600

    app.js commented somthing out

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ const HomePage = ()=>{
 
 
   const [ShowRegisterForm,setShowRegisterForm]=useState(register==1?true:false)
+  // const [ShowRegisterForm,setShowRegisterForm]=useState(register==1?true:false)
   return(
     <>
         <Nav setShowRegisterForm={setShowRegisterForm} />
