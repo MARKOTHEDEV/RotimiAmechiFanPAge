ee6ee633d33d5cffba7a7ce40640eb9adb3f13b7
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-02 09:45:18 -0600

    added etra content and some props with a little logic

--- src/Components/Alert.jsx
+++ src/Components/Alert.jsx
@@ -1,15 +1,18 @@
 
 
 
-const Alert=({message,show})=>{
-    console.log(message)
+const Alert=({message,show,setShowpopUp})=>{
+    // console.log(message)
 
     return (
   <div className="alert alert-primary" style={{
       zIndex:"1000",position:"fixed",top:0,left:0,backgroundColor:"#0b0016",padding:"1rem",borderRadius:"10px",
-      display:`${show}`
+      display:`${show}`,minWidth:"700px",justifyContent:"space-between"
       }}>
      <h3 style={{color:"whitesmoke"}}>{message}</h3>
+     <i class="fas fa-times-circle" style={{color:"white",fontSize:'1.4rem',cursor:"pointer"}}
+      onClick={(e)=>setShowpopUp(false)}
+     ></i>
    </div>
     )
 }
